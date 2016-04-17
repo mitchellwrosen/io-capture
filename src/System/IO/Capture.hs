a5aff8e09d05fbec9c624400721adf91194e2f23
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-04-16 21:02:44 -0400

    add captureStream

--- src/System/IO/Capture.hs
+++ src/System/IO/Capture.hs
@@ -1,46 +1,75 @@
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module System.IO.Capture (capture) where
+module System.IO.Capture
+  ( capture
+  , captureStream
+  ) where
 
 import Control.Exception
 import Control.Monad
 import System.Posix.IO
-import System.Posix.Process.ByteString
-import System.Posix.Types              (ByteCount, Fd, ProcessID)
+import System.Posix.Process
+import System.Posix.Types (ByteCount, Fd)
 
+import qualified Data.ByteString.Lazy      as LBS
 import qualified Data.ByteString.Streaming as S
 
 -- For haddocks
-import Data.ByteString.Streaming (toLazy_)
+import Data.ByteString.Streaming (stdout)
 
--- | Capture an IO action's @stdout@, @stderr@, and any thrown exception. Also
--- returns the pid of the spawned child.
+-- | Capture an IO action's @stdout@, @stderr@, and any thrown exception. Waits
+-- for the action to complete before returning.
 --
 -- @
--- import System.IO
--- import qualified Streaming.ByteString as S
---
--- > (out, _, _, _) <- capture ('putStrLn' "foo")
--- > S.'toLazy_' out
+-- > (out, _, _, _) <- 'capture' ('putStrLn' "foo")
+-- > 'print' out
 -- \"foo\"
 --
--- > (_, err, _, _) <- capture ('System.IO.hPutStrLn' 'System.IO.stderr' "bar")
--- > S.'toLazy_' err
+-- > (_, err, _, _) <- 'capture' ('System.IO.hPutStrLn' 'System.IO.stderr' \"bar\")
+-- > 'print' err
 -- \"bar\"
 --
--- > (_, _, exc, _) <- capture 'undefined'
--- > S.'toLazy_' exc
+-- > (_, _, exc, _) <- 'capture' 'undefined'
+-- > 'print' exc
 -- \"Prelude.undefined\"
 -- @
-capture
+capture :: IO a -> IO (LBS.ByteString, LBS.ByteString, LBS.ByteString, Maybe ProcessStatus)
+capture act = do
+  (out, err, exc, waitpid) <- captureStream act
+  status <- waitpid
+  (,,,)
+    <$> S.toLazy_ out
+    <*> S.toLazy_ err
+    <*> S.toLazy_ exc
+    <*> pure status
+
+-- | Stream an IO action's @stdout@, @stderr@, and any thrown exception. Also
+-- returns an action that @wait@s on the spawn child. This must be called
+-- eventually, otherwise the child will remain a zombie.
+--
+-- @
+-- import qualified Data.ByteString.Streaming as S
+--
+-- > let action = 'putStrLn' \"foo\" >> 'Control.Concurrent.threadDelay' 1000000
+-- > (out, _, _, waitpid) <- 'captureStream' ('Control.Monad.replicateM_' 5 action)
+-- > S.'stdout' out
+-- foo
+-- foo
+-- foo
+-- foo
+-- foo
+-- > waitpid
+-- Just ('Exited' 'System.Exit.ExitSuccess')
+-- @
+captureStream
   :: IO a
   -> IO ( S.ByteString IO ()
         , S.ByteString IO ()
         , S.ByteString IO ()
-        , ProcessID
+        , IO (Maybe ProcessStatus)
         )
-capture act = do
+captureStream act = do
   (out_r, out_w) <- createPipe
   (err_r, err_w) <- createPipe
   (exc_r, exc_w) <- createPipe
@@ -67,7 +96,11 @@ capture act = do
   err_rh <- fdToHandle err_r
   exc_rh <- fdToHandle exc_r
 
-  pure (S.hGetContents out_rh, S.hGetContents err_rh, S.hGetContents exc_rh, pid)
+  pure ( S.hGetContents out_rh
+       , S.hGetContents err_rh
+       , S.hGetContents exc_rh
+       , getProcessStatus True True pid
+       )
 
 fdWriteAll :: Fd -> String -> IO ()
 fdWriteAll fd str0 = go str0 (fromIntegral (length str0))
